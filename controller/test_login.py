a5c5bcb0ec5aff8f17c0958c007d6c979eb7291d
Simon Mwangi <mpsimon14@gmail.com>
2018-05-30 01:16:52 +0300

    [Feature 157952475]A test to make sure username field is not blank

--- controller/test_login.py
+++ controller/test_login.py
@@ -3,6 +3,12 @@ import pytest
 class Test_Login:
     
     #Ensures that the login page loads correctly
-    def test_login_loads {
+    def test_login_loads(self):
         pass
-    }
+    
+
+    #Test to ensure username is not blank
+    def test_username_is_not_blank(self):
+        pass
+    
+    
