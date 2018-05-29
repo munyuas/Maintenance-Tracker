3f10140915135ffda710ad3c8d07406da555d039
Simon Mwangi <mpsimon14@gmail.com>
2018-05-30 02:02:36 +0300

    [Feature #157953639] Test to check if username is not blank

--- controller/test_signup.py
+++ controller/test_signup.py
@@ -6,3 +6,7 @@ class Test_Login(unittest.TestCase):
     #Ensures that the signup page loads correctly
     def test_signup_loads(self):
         pass    
+
+    #Test to ensure username field is not blank
+    def test_username_is_not_blank(self):
+        pass
