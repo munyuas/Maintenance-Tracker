dc99bc71108a84ab093b40944802d42a0a6bd7cb
Simon Mwangi <mpsimon14@gmail.com>
2018-05-30 01:18:39 +0300

    [Feature 157952475]A test to make sure password field is not blank

--- controller/test_login.py
+++ controller/test_login.py
@@ -4,11 +4,16 @@ class Test_Login:
     
     #Ensures that the login page loads correctly
     def test_login_loads(self):
-        pass
-    
+        pass    
 
-    #Test to ensure username is not blank
+    #Test to ensure username field is not blank
     def test_username_is_not_blank(self):
         pass
+    
+    #Test to make sure the email field is not blank
+    def test_password_is_not_blank(self):
+        pass
+    
+    
     
     
