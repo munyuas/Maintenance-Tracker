a169b7e5a1ba00046bb134ad3119c19fda214830
Simon Mwangi <mpsimon14@gmail.com>
2018-05-30 08:44:18 +0300

    [Feature #157958310] Test for making sure the homepage loads correctly

---
+++ controller/tests/test_tracker.py
@@ -0,0 +1,20 @@
+from app import app
+import unittest
+
+
+
+class FlaskTestCase(unnittest.TestCase):
+    
+    #Ensures that the login page loads correctly
+    def test_index_page(self):
+        tester = app.test_client(self)
+        response = tester.get('/index', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    
+    
+if __name__ == '__main__':
+unittest.main()
+    
+    
+    
+    
