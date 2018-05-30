781ccf0a34e10df5a09c3654eacb3d0e919ac194
Simon Mwangi <mpsimon14@gmail.com>
2018-05-30 08:55:50 +0300

    [Feature #157958310] Test for making sure the sign in page loads correctly

--- controller/tests/test_tracker.py
+++ controller/tests/test_tracker.py
@@ -5,11 +5,20 @@ import unittest
 
 class FlaskTestCase(unnittest.TestCase):
     
-    #Ensures that the login page loads correctly
+    #Ensures that Flask Loads correctly
     def test_index_page(self):
         tester = app.test_client(self)
         response = tester.get('/index', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+        
+      #Ensures that the login page loads correctly
+    def test_signin_page_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/signin', content_type='html/text')
+        self.assertTrue (b'Sign In' in response.data)
+        
+    
+    
     
     
 if __name__ == '__main__':
